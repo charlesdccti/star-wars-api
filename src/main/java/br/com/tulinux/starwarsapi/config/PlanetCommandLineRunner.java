0e7d76257eefbb5382efdc57e3c33399565fa765
Deyve Vieira <deyvedvm@hotmail.com>
2018-05-10 00:31:49 -0300

    Command Line Runner

---
+++ src/main/java/br/com/tulinux/starwarsapi/config/PlanetCommandLineRunner.java
@@ -0,0 +1,54 @@
+package br.com.tulinux.starwarsapi.config;
+
+import br.com.tulinux.starwarsapi.models.Planet;
+import com.google.gson.Gson;
+import com.mashape.unirest.http.Unirest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * PlanetCommandLineRunner Component
+ * <p>
+ * Classe responsável por inserir dados iniciais no MongoDB da API Star Wars
+ */
+@Component
+public class PlanetCommandLineRunner implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlanetCommandLineRunner.class);
+
+    private final MongoTemplate mongoTemplate;
+
+    @Autowired
+    public PlanetCommandLineRunner(MongoTemplate mongoTemplate) {
+
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        Planet planet;
+
+        LOGGER.info("Inserindo 10 planetas no MongoDB");
+
+        for (int i = 1; i <= 10; i++) {
+
+            String planetString = Unirest.get("https://swapi.co/api/planets/{id}")
+                    .routeParam("id", String.valueOf(i))
+                    .asJson()
+                    .getBody()
+                    .toString();
+
+            planet = new Gson().fromJson(planetString, Planet.class);
+
+            mongoTemplate.save(planet, "planet");
+        }
+
+    }
+}
+
+
